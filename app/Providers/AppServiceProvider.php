65a57c769f401d2e5f6633a841acfe2b2e87a0b4
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-08-29 21:19:17 +0200

    Config Before Start

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configurateModels();
+        $this->configurateCommands();
+        $this->configurateURL();
+        // $this->configuratePulse();
+    }
+
+    private function configuratePulse(): void
+    {
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
+    }
+
+    private function configurateModels(): void
+    {
+        Model::automaticallyEagerLoadRelationships();
+        // Model::unguard();
+        Model::shouldBeStrict(! app()->isProduction());
+    }
+
+    private function configurateCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    private function configurateURL(): void
+    {
+        if (app()->isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
